26164d5fea9699cf182155c94eafdc2567de1d5d
CaloMorell <oscarmorell07@yahoo.com>
2024-02-01 12:12:39 -0400

    0. Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
+// script that reads and prints the content of a file.
+let aFile = process.argv[2];
 const fs = require('fs');
-
-// Verifica si se proporciona el número correcto de argumentos
-if (process.argv.length !== 3) {
-console.error('Uso: node readfile.js <ruta_del_archivo>');
-process.exit(1);
-}
-
-const filePath = process.argv[2];
-
-// Lee el contenido del archivo en utf-8
-fs.readFile(filePath, 'utf-8', (err, data) => {
-if (err) {
-    console.error('Error al leer el archivo:', err);
-} else {
-    console.log('Contenido del archivo:');
-    console.log(data);
-}
+fs.readFile(aFile, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    aFile = data;
+    console.log(aFile);
+  }
 });
