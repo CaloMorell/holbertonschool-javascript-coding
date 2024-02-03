f205371803ad50b89149d673dd88cb8da6440830
CaloMorell <oscarmorell07@yahoo.com>
2024-02-02 21:54:08 -0400

    update

--- Node_JS_basic/8-main.js
+++ Node_JS_basic/8-main.js
@@ -1,4 +1,4 @@
-curl localhost:1245 && echo ""
+curl 1245 && echo ""
 curl localhost:1245/students && echo ""
 curl localhost:1245/students/SWE && echo ""
 curl localhost:1245/students/French -vvv && echo ""
